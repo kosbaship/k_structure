5c6aa1a10d70ee8385f01c4c9a882c40087114c1
Mohamed M. Kosba <mohamed.kosba96@gmail.com>
2021-09-04 18:31:41 +0200

    Prepare For Checking Network & Local Cache

--- lib/repository/app_repository.dart
+++ lib/repository/app_repository.dart
@@ -1,18 +1,30 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:k_structure/data_source/api_helper.dart';
 import 'package:k_structure/model/post_model.dart';
 import 'package:k_structure/repository/base_repository.dart';
 import 'package:k_structure/shared/app_endpoints.dart';
 
 class AppRepository with BaseRepository {
-  static AppRepository _instance = AppRepository();
+  static AppRepository _instance =
+      AppRepository(remoteDataSource: ApiHelper.getInstance);
   static AppRepository get getInstance => _instance;
+
+  final ApiHelper remoteDataSource;
+  // final CacheHelper localDataSource;
+  // final NetworkInfo networkInfo;
+
+  AppRepository({
+    @required this.remoteDataSource,
+    // this.localDataSource,
+    // this.networkInfo,
+  });
 
   @override
   Future<List<Post>> getPosts() async {
-    String resultJSONStringContainsListOfDynamic = await ApiHelper.getInstance
-        .sendApiRequest(urlWithEndPoint: kPostsService);
+    String resultJSONStringContainsListOfDynamic =
+        await remoteDataSource.sendApiRequest(urlWithEndPoint: kPostsService);
 
     return List<Post>.from(jsonDecode(resultJSONStringContainsListOfDynamic)
         .map((postElement) => Post.fromJson(postElement)));
--- lib/shared/app_configration.dart
+++ lib/shared/app_configration.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:k_structure/data_source/api_helper.dart';
 import 'package:k_structure/repository/app_repository.dart';
 
 configureDependencies() async {
   WidgetsFlutterBinding.ensureInitialized();
-  ApiHelper.getInstance;
   AppRepository.getInstance;
 }
